b80ef6c0dca89cb3835188ea65980ab96ce96c8c
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-03-24 00:09:22 +0530

    Session-3-Assignment-4 Added

--- Session3Assignment4/src/main/scala/LinkedList.scala
+++ Session3Assignment4/src/main/scala/LinkedList.scala
@@ -1,19 +1,21 @@
 package com.knoldus
-
 import scala.annotation.tailrec
 
 case class Node[T](data: T, var next: Node[T])
 class LinkedList[T] {
 
-    var head: Node[T] =  null
+  var head: Node[T] = null
 
-    def insertElementAtBeginning(elementToInsert: T): Unit = {
-      val newNode = new Node[T](elementToInsert, head)
-      head = newNode
-    }
+  //method to insert element at beginning
+  def insertElementAtBeginning(elementToInsert: T): Unit = {
+    val newNode = new Node[T](elementToInsert, head)
+    head = newNode
+  }
 
+  //method to insert element at end
   def insertElementAtEnd(elementToInsert: T): Unit = {
     val newNode = new Node[T](elementToInsert, null)
+
     def insertHelper(currentNode: Node[T]): Unit = {
       if (head == null) {
         head = newNode
@@ -23,73 +25,63 @@ class LinkedList[T] {
       }
       else currentNode.next = newNode
     }
+
     insertHelper(head)
   }
 
-    def delete(data: T): Unit = {
-      deleteHelper(head, head.next)
-      @tailrec
-      def deleteHelper(previousNode: Node[T], currentNode: Node[T]): Unit = {
-        // To delete the first element
-        if (head == null) {
-          return
-        }
-        if (head.data == data) {
-          head = head.next
-          return
-        }
-        // To delete elements other than first element
-        if (currentNode == null) {
-          return
-        }
-        if (currentNode.data == data) {
-          previousNode.next = currentNode.next
-        } else {
-          deleteHelper(currentNode, currentNode.next)
-        }
+  //method to delete element
+  def delete(data: T): Unit = {
+    deleteHelper(head, head.next)
+
+    @tailrec
+    def deleteHelper(previousNode: Node[T], currentNode: Node[T]): Unit = {
+      // To delete the first element
+      if (head == null) {
+        return
+      }
+      if (head.data == data) {
+        head = head.next
+        return
+      }
+      // To delete elements other than first element
+      if (currentNode == null) {
+        return
+      }
+      if (currentNode.data == data) {
+        previousNode.next = currentNode.next
+      } else {
+        deleteHelper(currentNode, currentNode.next)
+      }
+    }
+  }
+
+  //method to traverse linked list
+  def traverse(): Unit = {
+    @tailrec
+    def traverseHelper(currentNode: Node[T]): Unit = {
+      if (currentNode != null) {
+        //println(currentNode.data)
+        traverseHelper(currentNode.next)
       }
     }
 
+    traverseHelper(head)
+  }
 
-    def traverse(): Unit = {
-     @tailrec
-      def traverseHelper(currentNode: Node[T]): Unit = {
-        if (currentNode != null) {
-          //println(currentNode.data)
-          traverseHelper(currentNode.next)
-        }
+  //method to search an element
+  def search(data: T): String = {
+    @tailrec
+    def searchHelper(current: Node[T]): String = {
+      if (current == null) {
+        "Data not found"
+      } else if (current.data == data) {
+        s"Data Found ${current.data}"
+      } else {
+        searchHelper(current.next)
       }
-      traverseHelper(head)
     }
 
-    def search(data: T): Option[Node[T]] = {
-      @tailrec
-      def searchHelper(current: Node[T]): Option[Node[T]] = {
-        if (current == null) {
-          None
-        } else if (current.data == data) {
-          println(s"Data Found ${current.data}")
-          Some(current)
-        } else {
-          searchHelper(current.next)
-        }
-      }
-      searchHelper(head)
-    }
+    searchHelper(head)
+  }
 }
-object LinkedList extends App {
-  val obj = new LinkedList[Int]
-//  obj.insertElementAtEnd(12)
-//  obj.insertElementAtEnd(15)
-//  obj.insertElementAtEnd(16)
-//  obj.insertElementAtEnd(19)
-//  obj.insertElementAtBeginning(90)
-//  obj.insertElementAtBeginning(80)
-//  obj.insertElementAtBeginning(60)
-//  println("Before")
-//  obj.traverse()
-//  obj.delete(90)
-//  println("after")
-//  obj.traverse()
-//  obj.search(16)
-}
+
--- Session3Assignment4/src/test/scala/Session3Assignment4Test.scala
+++ Session3Assignment4/src/test/scala/Session3Assignment4Test.scala
@@ -6,59 +6,57 @@ class Session3Assignment4Test extends AnyFunSuite {
   val linkedListStringObject = new LinkedList[String]
   val linkedListLongObject = new LinkedList[Long]
 
-  test("To insert element at beginning in integer") {
+  test("To insert element in integer") {
     linkedListIntObject.insertElementAtBeginning(10)
     linkedListIntObject.insertElementAtBeginning(20)
-    linkedListIntObject.traverse()
-    assert(linkedListIntObject.head.data == 20)
-  }
-  test("To insert element at beginning in string") {
-    linkedListStringObject.insertElementAtBeginning("Hello")
-    linkedListStringObject.insertElementAtBeginning("World")
-    linkedListStringObject.traverse()
-    assert(linkedListStringObject.head.data == "World")
-  }
-  test("To insert element at beginning in long") {
-    linkedListLongObject.insertElementAtBeginning(89997123)
-    linkedListLongObject.insertElementAtBeginning(67544649)
-    linkedListLongObject.traverse()
-    assert(linkedListLongObject.head.data == 67544649)
-  }
-  test("To insert element at end in integer") {
     linkedListIntObject.insertElementAtEnd(30)
     linkedListIntObject.insertElementAtEnd(40)
-    linkedListIntObject.traverse()
+    linkedListIntObject.traverse() // 20 -> 10 -> 30 -> 40
     assert(linkedListIntObject.head.next.next.data == 30)
+    assert(linkedListIntObject.head.data == 20)
   }
-  test("To insert element at end in string") {
+  test("To insert element in string") {
+    linkedListStringObject.insertElementAtBeginning("Hello")
+    linkedListStringObject.insertElementAtBeginning("World")
     linkedListStringObject.insertElementAtEnd("Happy")
     linkedListStringObject.insertElementAtEnd("Day")
-    linkedListStringObject.traverse()
+    linkedListStringObject.traverse() // World -> Hello -> Happy -> Day
     assert(linkedListStringObject.head.next.next.data == "Happy")
+    assert(linkedListStringObject.head.data == "World")
   }
-  test("To insert element at end in long") {
+  test("To insert element in long") {
+    linkedListLongObject.insertElementAtBeginning(89997123)
+    linkedListLongObject.insertElementAtBeginning(67544649)
     linkedListLongObject.insertElementAtEnd(923457123)
     linkedListLongObject.insertElementAtEnd(578744649)
-    linkedListLongObject.traverse()
+    linkedListLongObject.traverse() // 67544649 -> 89997123 -> 923457123 -> 578744649
     assert(linkedListLongObject.head.next.next.data == 923457123)
+    assert(linkedListLongObject.head.data == 67544649)
   }
   test("To delete an element in integer") {
+    linkedListIntObject.insertElementAtBeginning(10)
+    linkedListIntObject.insertElementAtBeginning(20)
     linkedListIntObject.delete(10)
     linkedListIntObject.traverse()
     assert(linkedListIntObject.head.data == 20)
   }
   test("To delete an element in string") {
+    linkedListStringObject.insertElementAtBeginning("Hello")
+    linkedListStringObject.insertElementAtBeginning("World")
     linkedListStringObject.delete("Hello")
     linkedListStringObject.traverse()
     assert(linkedListStringObject.head.data == "World")
   }
   test("To delete an element in long") {
+    linkedListLongObject.insertElementAtBeginning(89997123)
+    linkedListLongObject.insertElementAtBeginning(67544649)
     linkedListLongObject.delete(89997123)
     linkedListLongObject.traverse()
     assert(linkedListLongObject.head.data == 67544649)
   }
   test("To search an element in integer") {
-    assert(linkedListIntObject.search(30).get.data == Some(30))
+    assert(linkedListIntObject.search(80) == "Data not found")
+    assert(linkedListIntObject.search(30) == "Data Found 30")
   }
   test("To search an element in string") {
     assert(linkedListStringObject.search("Day") == "Data Found Day")
